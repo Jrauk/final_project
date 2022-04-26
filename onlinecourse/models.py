86cf84c802484373b2924650e66fce77460b10c2
Jrauk <103428723+Jrauk@users.noreply.github.com>
2022-04-26 20:51:04 +0200

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -110,8 +110,9 @@ class Enrollment(models.Model):
     # Other fields and methods you would like to design
 class Question(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='questions')
-    question_text = models.CharField(max_length=200, default='Question text')
+    question_text = models.CharField(max_length=200, default='Question text', null=False)
     grade = models.IntegerField(default=1)
+    course = models.ManyToManyField(Course)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
@@ -130,8 +131,8 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
-    choice_text = models.CharField(max_length=255, default='Answer')
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=255, null=False, default='Answer')
     is_correct = models.BooleanField()
     
     def __str__(self):
